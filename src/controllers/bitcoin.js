0797dbf59a0b4136b3bb63f44643c420f63b0a8f
Cody Windeknecht <lhg614@mocs.utc.edu>
2018-03-09 13:49:16 -0500

    Added Return Price for a Date by Query
    controllers/bitcoin.js --- Conditional if there was a query for a date
    
     models/bitcoin.js --- add getDate function that returns a promise with the bitcoin information about a date provided in the query.

--- src/controllers/bitcoin.js
+++ src/controllers/bitcoin.js
@@ -6,34 +6,48 @@ const config = require('../config.js');
 const STATUS_USER_ERROR = config.status.user_error;
 const STATUS_OKAY = config.status.okay;
 
-const { getCurrent, getPrevious } = require('../models/bitcoin.js');
+const { getCurrent, getPrevious, getDate } = require('../models/bitcoin.js');
 
 router.get('/compare', (req, res) => {
-let currentPrice = 0;
-let previousPrice = 0;
-  getCurrent()
-    .then(current => currentPrice = current.bpi.USD.rate_float)
-    .then(getPrevious)
-    .then(previous => {
+  if (!req.query.date) {
+    let currentPrice = 0;
+    let previousPrice = 0;
+    getCurrent()
+      .then(current => (currentPrice = current.bpi.USD.rate_float))
+      .then(getPrevious)
+      .then(previous => {
         previousPrice = Object.values(previous.bpi)[0];
         const rPrev = round(previousPrice);
         const rCurr = round(currentPrice);
-        const rDiff = round(rCurr - rPrev)
+        const rDiff = round(rCurr - rPrev);
         res.send({
-            currentPrice: '$' + rCurr,
-            previousPrice: '$' + rPrev,
-            differnce: '$' + rDiff
+          currentPrice: '$' + rCurr,
+          previousPrice: '$' + rPrev,
+          differnce: '$' + rDiff,
         });
+      })
+      .catch(err => {
+        res.status(STATUS_USER_ERROR);
+        res.send({ err });
+      });
+  } 
+  else {
+    getDate(req.query.date)
+    .then(date => {
+        const price = round(Object.values(date.bpi)[0])
+        res.status(STATUS_OKAY);
+        res.send({date: req.query.date, price: '$' + price});
     })
     .catch(err => {
-      res.status(STATUS_USER_ERROR);
-      res.send({ err });
-    });
+        res.status(STATUS_USER_ERROR);
+        res.send({ err });
+      });
+  }
 });
 
 function round(num) {
-    const number = Number(num);
-    return Math.ceil(number * 100) / 100
+  const number = Number(num);
+  return Math.ceil(number * 100) / 100;
 }
 
 module.exports = router;
--- src/models/bitcoin.js
+++ src/models/bitcoin.js
@@ -3,8 +3,9 @@ const config = require('../config.js');
 
 const URL_CURRENT = 'https://api.coindesk.com/v1/bpi/currentprice.json';
 const URL_PREVIOUS = 'https://api.coindesk.com/v1/bpi/historical/close.json?for=yesterday';
+const URL_DATE = 'https://api.coindesk.com/v1/bpi/historical/close.json?'
 
-function getCurrent(term) {
+function getCurrent() {
   return new Promise((resolve, reject) => {
     fetch(URL_CURRENT)
       .then(res => res.json())
@@ -17,11 +18,13 @@ function getCurrent(term) {
   });
 }
 
-function getPrevious(ids) {
+function getPrevious() {
+    console.log('firing1')
   return new Promise((resolve, reject) => {
     fetch(URL_PREVIOUS)
       .then(res => res.json())
       .then(previous => {
+        console.log('firing2')
         resolve(previous);
       })
       .catch(err => {
@@ -30,4 +33,18 @@ function getPrevious(ids) {
   });
 }
 
-module.exports = { getCurrent, getPrevious };
+function getDate(date) {
+    const url = URL_DATE + 'start=' + date + '&end=' + date;
+    return new Promise((resolve, reject) => {
+        fetch(url)
+          .then(res => res.json())
+          .then(date => {
+            resolve(date);
+          })
+          .catch(err => {
+            reject(err);
+          });
+      });
+}
+
+module.exports = { getCurrent, getPrevious, getDate };
