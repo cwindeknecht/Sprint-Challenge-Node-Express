592ab5df2d4beac6ec2e3668a9ce3a225cdc2369
Cody Windeknecht <lhg614@mocs.utc.edu>
2018-03-09 11:40:05 -0500

    App.js Finished
    app.js --- Imported required tools/files, applied unwritten middleware, server information.

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const config = require('./config.js');
+const bitcoinController = require('./controllers/bitcoin.js');
+
+const app = express();
+const PORT = config.port;
+
+app.use(bitcoinController);
+
+app.listen(PORT, err => {
+  if (err) {
+    console.log(`Error starting server: ${err}`);
+  } else {
+    console.log(`Server listening on port ${PORT}`);
+  }
+});
